a12ebe58bc65ccfb8d0750d4ecfc267b6063b6e5
Turner <christian.turner3@AGR.GC.CA>
2022-08-31 10:23:33 -0400

    create tls certificates

--- terraform/tls.tf
+++ terraform/tls.tf
@@ -1,22 +1,15 @@
-resource "tls_private_key" "rootCA_key" {
+# ---------------------------------------------------------------------------------------------------------------------
+#  CREATE A CA CERTIFICATE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "tls_private_key" "ca" {
   algorithm   = var.tls.algorithm
   ecdsa_curve = var.tls.ecdsa_curve
 }
 
-resource "tls_cert_request" "rootCA_req" {
-  private_key_pem = tls_private_key.rootCA_key.private_key_pem
-  subject {
-    common_name         = var.tls.common_name
-    country             = var.tls.country
-    locality            = var.tls.locality
-    organization        = var.tls.organization
-    organizational_unit = var.tls.organizational_unit
-    province            = var.tls.province
-  }
-}
-
-resource "tls_self_signed_cert" "rootCA_pem" {
-  private_key_pem       = tls_private_key.rootCA_key.private_key_pem
+resource "tls_self_signed_cert" "ca" {
+  private_key_pem       = tls_private_key.ca.private_key_pem
+  key_algorithm     = tls_private_key.ca.algorithm
   is_ca_certificate     = true
   set_authority_key_id  = true
   set_subject_key_id    = true
@@ -35,54 +28,55 @@ resource "tls_self_signed_cert" "rootCA_pem" {
     "crl_signing"
   ]
 }
-
+# Store the CA public key in a file.
 resource "local_file" "rootca_pem" {
-  content  = tls_self_signed_cert.rootCA_pem.cert_pem
+  content  = tls_self_signed_cert.ca.cert_pem
   filename = "${path.module}/rootCA.pem"
 }
 
-# 8. Create the client key:
-# openssl ecparam -genkey -name secp384r1 -out server.key
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A TLS CERTIFICATE SIGNED USING THE CA CERTIFICATE
+# ---------------------------------------------------------------------------------------------------------------------
 
-resource "tls_private_key" "server_key" {
+resource "tls_private_key" "cert" {
   algorithm   = var.tls.algorithm
   ecdsa_curve = var.tls.ecdsa_curve
 }
 
-# 10. Create the client CSR:
-# openssl req -new -sha512 -nodes -key server.key -out server.csr -config server_request.config
-resource "tls_cert_request" "server_csr" {
-  private_key_pem = tls_private_key.server_key.private_key_pem
+resource "local_file" "server_key" {
+  content  = tls_private_key.cert.private_key_pem
+  filename = "${path.module}/server.key"
+}
+
+resource "tls_cert_request" "cert" {
+  key_algorithm   = "tls_private_key.cert.algorithm
+  private_key_pem = "tls_private_key.cert.private_key_pem
 
   subject {
-    common_name         = "Acme.com"
-    country             = "US"
-    organization        = "Acme Inc."
-    organizational_unit = "Innovation"
+    common_name         = var.tls.common_name
+    country             = var.tls.country
+    locality            = var.tls.locality
+    organization        = var.tls.organization
+    organizational_unit = var.tls.organizational_unit
+    province            = var.tls.province
   }
 }
 
-# 12. Create the CA signed client certificate: + create server.pem output file
-# openssl x509 -req -sha512 -days 365 -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.pem -extfile server_cert.config
-resource "tls_locally_signed_cert" "server_pem" {
-  cert_request_pem   = tls_cert_request.server_csr.cert_request_pem
-  ca_private_key_pem = tls_self_signed_cert.rootCA_pem.private_key_pem
-  ca_cert_pem        = tls_self_signed_cert.rootCA_pem.cert_pem
-  #tls_cert_request.server_csr.private_key_pem
+resource "tls_locally_signed_cert" "cert" {
+  cert_request_pem = tls_cert_request.cert.cert_request_pem
+
+  ca_key_algorithm   = tls_private_key.ca.algorithm
+  ca_private_key_pem = tls_private_key.ca.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
 
   validity_period_hours = var.tls.validity_period_hours
-
   allowed_uses = [
     "digital_signature"
   ]
 }
-
-resource "local_file" "server_pem" {
-  content  = tls_locally_signed_cert.server_pem.ca_cert_pem
-  filename = "${path.module}/server.pem"
 }
 
-resource "local_file" "server_key" {
-  content  = tls_private_key.server_key.private_key_pem
-  filename = "${path.module}/server.key"
+resource "local_file" "server_pem" {
+  content  = tls_locally_signed_cert.cert.cert_pem
+  filename = "${path.module}/server.pem"
 }
